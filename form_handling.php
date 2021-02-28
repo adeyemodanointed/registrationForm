f407f36903ff03f9d6e3859f3bff49cf977c7ec1
Peter Adeyemo <adeyemodanointed@yahoo.com>
2021-02-28 14:36:21 +0100

    Second design

--- form_handling.php
+++ form_handling.php
@@ -18,14 +18,18 @@ if(isset( $_POST['fullName']) && isset( $_POST['email'])  && isset( $_POST['phon
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration Form</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <form action="" method="POST">
-        <input type="text" name="fullName" placeholder="Enter your full name">
-        <input type="email" name="email" placeholder="Enter your email address">
-        <input type="tel" name="phone" placeholder="Enter your phone number">
-        <input type="text" name="address" placeholder="Enter your address">
-        <input type="submit" name="submit" value="SUBMIT">
-    </form>
+    <div>
+        <h3>MY REGISTRATION FORM</h3>
+        <form class="regForm" action="" method="POST">
+            <input type="text" name="fullName" placeholder="Enter your full name">
+            <input type="email" name="email" placeholder="Enter your email address">
+            <input type="tel" name="phone" placeholder="Enter your phone number">
+            <input type="text" name="address" placeholder="Enter your address">
+            <input type="submit" name="submit" value="SUBMIT" id="submit">
+        </form>
+    </div>
 </body>
 </html>
